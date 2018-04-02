fb75ac18a99da7db56ed3bc948248ca983f871a5
josephscade <leo.pourcelot@protonmail.com>
2018-04-02 11:14:49 +0200

    removed useless code (function returned value and ? behavour

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,26 @@ extern crate regex;
 
 fn main() {
     let args = parse_arguments();
-    if args.find_string != ""
-    {
-        let find_regex = regex::Regex::new(&format!(r"(?i)(?P<match>{})", args.find_string)).unwrap();
+    if args.find_string != "" {
+        let find_regex = regex::Regex::new(&format!(r"(?i)(?P<match>{})", args.find_string))
+            .unwrap();
         let init_path = std::path::Path::new("./");
 
         list_dir(init_path,
                  &find_regex,
                  &args);
-    }
-    else
-    {
+    } else {
         println!("No pattern specified! Please specify a pattern.");
     }
 }
 
-struct Arguments
-{
+struct Arguments {
     hidden_directories: bool,
     color: bool,
     find_string: String,
 }
 
-fn parse_arguments() ->Arguments
-{
+fn parse_arguments() -> Arguments {
     let mut use_color: bool = {
         match std::env::var_os("NO_COLOR") {
             None => true,
@@ -36,36 +32,24 @@ fn parse_arguments() ->Arguments
     let mut string: String = String::from("");
     let mut count: i32 = 0;
     let mut all_directories: bool = false;
-    for argument in std::env::args()
-    {
-        if count == 0
-        {
+    for argument in std::env::args() {
+        if count == 0 {
             count += 1;
-        }
-        else
-        {
-            if argument.get(0..1) == Some("-")
-            {
-                if argument.get(1..2) == Some("-")
-                {
+        } else {
+            if argument.get(0..1) == Some("-") {
+                if argument.get(1..2) == Some("-") {
                     match argument.as_ref() {
                         "--no-color" => use_color = false,
                         _ => println!("Argument \"{}\" not recognized", argument),
                     }
-                }
-                else
-                {
-                    for letter in argument.chars()
-                    {
-                        if letter.to_string() == "a"
-                        {
+                } else {
+                    for letter in argument.chars() {
+                        if letter.to_string() == "a" {
                             all_directories = true;
                         }
                     }
                 }
-            }
-            else if string == ""
-            {
+            } else if string == "" {
                 string = argument;
             }
         }
@@ -77,33 +61,44 @@ fn parse_arguments() ->Arguments
     }
 }
 
-fn list_dir(dir_name: &std::path::Path, regex: &regex::Regex, args: &Arguments) -> std::io::Result<()>
-{
+fn list_dir(
+    dir_name: &std::path::Path,
+    regex: &regex::Regex,
+    args: &Arguments,
+) {
 
     if dir_name.is_dir() {
-        for entry in std::fs::read_dir(dir_name)? {
-            let entry = entry?;
-            let can_walk_trought: bool = entry.path().components().last().unwrap().as_os_str().to_str().unwrap().get(0..1) != Some(".");
-            if can_walk_trought || args.hidden_directories
-            {
+        for entry in std::fs::read_dir(dir_name).unwrap() {
+            let entry = entry.unwrap();
+            let can_walk_trought: bool = entry
+                .path()
+                .components()
+                .last()
+                .unwrap()
+                .as_os_str()
+                .to_str()
+                .unwrap()
+                .get(0..1) != Some(".");
+            if can_walk_trought || args.hidden_directories {
                 if regex.is_match(&format!("{}", entry.path().display())) {
                     let mut print_string;
                     if args.color {
-                            print_string = String::from(regex.replace_all(&format!("{}", entry.path().display()), "\x1B[31m$match\x1B[0m"));
-                    }
-                    else {
+                        print_string = String::from(regex.replace_all(
+                            &format!("{}", entry.path().display()),
+                            "\x1B[31m$match\x1B[0m",
+                        ));
+                    } else {
                         print_string = format!("{}", entry.path().display());
                     }
                     println!("{}", print_string);
                 }
                 if entry.path().is_dir() {
-                    
-                    list_dir(&entry.path(),
+                    list_dir(
+                        &entry.path(),
                         &regex,
                         &args);
                 }
             }
         }
     }
-    Ok(())
 }
